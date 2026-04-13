17a350949c4cc383efc9a5c2a3edd28c60c1ffd0
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-13 17:21:42 +0900

    fix : 카테고리 이름 dto에서 Size로 검증

--- src/main/java/com/example/allinmarket/admin/category/dto/CategoryCreateRequest.java
+++ src/main/java/com/example/allinmarket/admin/category/dto/CategoryCreateRequest.java
@@ -2,14 +2,15 @@ package com.example.allinmarket.admin.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Size;
 
-public record CategoryCreateRequest (
+public record CategoryCreateRequest(
         @NotBlank(message = "카테고리 이름은 필수입니다.")
-        @Length(max = 50, message = "카테고리 이름은 50자를 초과할 수 없습니다.")
+        @Size(max = 50, message = "카테고리 이름은 50자를 초과할 수 없습니다.")
         String name,
 
         @PositiveOrZero(message = "순서는 0 또는 양수로 지정해 주세요")
         int sortOrder
-) {}
+) {
+}
 
