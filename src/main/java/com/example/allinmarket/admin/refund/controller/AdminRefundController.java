77333a74420048eaff2d9cda6d5b75f939bf4d41
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-13 20:01:46 +0900

    feat : 관리자 환불 요청 조회, 승인 컨트롤러 메서드 추가

--- src/main/java/com/example/allinmarket/admin/refund/controller/AdminRefundController.java
+++ src/main/java/com/example/allinmarket/admin/refund/controller/AdminRefundController.java
@@ -1,9 +1,17 @@
 package com.example.allinmarket.admin.refund.controller;
 
+import com.example.allinmarket.admin.refund.dto.request.AuthorizeRefundRequest;import com.example.allinmarket.admin.refund.dto.response.AuthorizeRefundResponse;
 import com.example.allinmarket.admin.refund.service.AdminRefundService;
+import com.example.allinmarket.common.enums.SuccessEnum;
+import com.example.allinmarket.common.response.ApiResponse;
+import com.example.allinmarket.common.response.PageResponse;
+import com.example.allinmarket.common.security.SecurityUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.kafka.shaded.com.google.protobuf.Api;import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,5 +20,24 @@ public class AdminRefundController {
 
     private final AdminRefundService adminRefundService;
 
+    @GetMapping
+    public ResponseEntity<ApiResponse<PageResponse<AuthorizeRefundResponse>>> findAll(
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.ASC) Pageable pageable
+    ) {
+        Long adminId = SecurityUtils.getCurrentUserId();
+        return ResponseEntity.ok(ApiResponse.success(SuccessEnum.READ_SUCCESS, adminRefundService.findAll(adminId, pageable)));
+    }
+
+    @PutMapping
+    public ResponseEntity<ApiResponse<AuthorizeRefundResponse>> authorizeRefund(
+            @PathVariable Long refundId,
+            @RequestBody AuthorizeRefundRequest request
+    ) {
+        Long adminId = SecurityUtils.getCurrentUserId();
+        return ResponseEntity.ok(ApiResponse.success(
+                SuccessEnum.UPDATE_SUCCESS,
+                adminRefundService.authorizeRefund(adminId, refundId, request)
+        ));
+    }
 
 }
