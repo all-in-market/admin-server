e6c638e9b0f855970cd74374cf465542cd79e52d
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-13 16:15:06 +0900

    feat : 관리자 카테고리 조회 메서드 생성

--- src/main/java/com/example/allinmarket/admin/category/service/AdminCategoryService.java
+++ src/main/java/com/example/allinmarket/admin/category/service/AdminCategoryService.java
@@ -1,4 +1,31 @@
 package com.example.allinmarket.admin.category.service;
 
+import com.example.allinmarket.admin.repository.AdminRepository;
+import com.example.allinmarket.buyer.category.dto.CategoryDetailResponse;
+import com.example.allinmarket.common.enums.ErrorEnum;
+import com.example.allinmarket.common.exception.BaseException;
+import com.example.allinmarket.domain.category.entity.Category;
+import com.example.allinmarket.domain.category.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AdminCategoryService {
+
+    private final AdminRepository adminRepository;
+    private final CategoryRepository categoryRepository;
+
+    public List<CategoryDetailResponse> getCategories(Long adminId) {
+
+        if (!adminRepository.existsByIdAndDeletedAtIsNull(adminId)) {
+            throw new BaseException(ErrorEnum.ADMIN_NOT_FOUND);
+        }
+
+        return categoryRepository.findAllAndDeletedAtIsNull().stream()
+                .map(CategoryDetailResponse::from)
+                .toList();
+    }
 }
