ec8510c4966cfc4b9cb10fd46d44deeeae4404f5
LeeJun14 <777ljy777@gmail.com>
2026-04-13 20:04:25 +0900

    feat: 관리자 환불 api 구현

--- src/main/java/com/example/allinmarket/admin/refund/service/AdminRefundService.java
+++ src/main/java/com/example/allinmarket/admin/refund/service/AdminRefundService.java
@@ -1,7 +1,16 @@
 package com.example.allinmarket.admin.refund.service;
 
+import com.example.allinmarket.admin.refund.dto.request.AuthorizeRefundRequest;
+import com.example.allinmarket.admin.refund.dto.response.AuthorizeRefundResponse;
+import com.example.allinmarket.admin.repository.AdminRepository;
+import com.example.allinmarket.buyer.order.dto.response.OrderDetailResponse;
+import com.example.allinmarket.common.enums.ErrorEnum;
+import com.example.allinmarket.common.exception.BaseException;
+import com.example.allinmarket.common.response.PageResponse;
+import com.example.allinmarket.domain.refund.entity.Refund;
 import com.example.allinmarket.domain.refund.repository.RefundRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,5 +20,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class AdminRefundService {
 
     private final RefundRepository refundRepository;
+    private final AdminRepository adminRepository;
 
+    public PageResponse<AuthorizeRefundResponse> findAll(Long adminId, Pageable pageable) {
+
+        validationForbidden(adminId);
+
+        return PageResponse.register(
+                refundRepository.findAllBy(pageable)
+                        .map(AuthorizeRefundResponse::from)
+        );
+    }
+
+    public AuthorizeRefundResponse authorizeRefund(Long adminId, Long refundId, AuthorizeRefundRequest request) {
+
+        validationForbidden(adminId);
+
+        Refund refund = refundRepository.findById(refundId).orElseThrow(
+                () -> new BaseException(ErrorEnum.REFUND_NOT_FOUND)
+        );
+
+
+
+
+
+    }
+
+    private void validationForbidden(Long adminId) {
+        if (!adminRepository.existsByIdAndDeletedAtIsNull(adminId)) {
+            throw new BaseException(ErrorEnum.ADMIN_NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/example/allinmarket/domain/refund/repository/RefundRepository.java
+++ src/main/java/com/example/allinmarket/domain/refund/repository/RefundRepository.java
@@ -1,7 +1,10 @@
 package com.example.allinmarket.domain.refund.repository;
 
 import com.example.allinmarket.domain.refund.entity.Refund;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefundRepository extends JpaRepository<Refund,Long> {
+    Page<Refund> findAllBy(Pageable pageable);
 }
