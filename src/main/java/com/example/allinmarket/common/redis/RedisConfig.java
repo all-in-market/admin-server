12247cc03f74ac4707622386e4940b8a6f14f888
ginsengcandy <kimsparadise0202@gmail.com>
2026-04-30 13:47:51 +0900

    fix: test 환경에서 redis 연결 안함

--- src/main/java/com/example/allinmarket/common/redis/RedisConfig.java
+++ src/main/java/com/example/allinmarket/common/redis/RedisConfig.java
@@ -6,12 +6,14 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Profile("!test")
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
--- src/test/java/com/example/allinmarket/common/config/TestRedisConfig.java
+++ src/test/java/com/example/allinmarket/common/config/TestRedisConfig.java
@@ -4,6 +4,7 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @TestConfiguration
 public class TestRedisConfig {
@@ -11,5 +12,10 @@ public class TestRedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return Mockito.mock(RedisConnectionFactory.class);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        return Mockito.mock(RedisTemplate.class);
     }
 }
