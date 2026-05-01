e8d63135931a8de82005b050c75d6fc4b8467472
hyu1335 <hyuham1335@gmail.com>
2026-05-01 18:00:25 +0900

    feat: health check api 추가

--- src/main/java/com/example/allinmarket/common/config/SecurityConfig.java
+++ src/main/java/com/example/allinmarket/common/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers("/admin/auth/login").permitAll()
                         .requestMatchers("/admin/auth/logout").hasRole("ADMIN")
                         .anyRequest().hasRole("ADMIN")
